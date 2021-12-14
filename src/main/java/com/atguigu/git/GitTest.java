16fe910b6ee796ebf7ddaa281f7088f7fc40e1cc
ZhangWeichen <734676881@qq.com>
2021-12-14 09:20:43 +0800

    Idea Git test

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author zhangweichen
+ * @create 2021-12-14 9:14
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello, Git!");
+    }
+}
