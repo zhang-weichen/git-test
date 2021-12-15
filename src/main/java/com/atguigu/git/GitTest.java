ea6397ef3a0c4dc333adfbb1abeae9d0e7bef21f
ZhangWeichen <734676881@qq.com>
2021-12-15 10:08:39 +0800

    Idea Git test

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -6,8 +6,8 @@ package com.atguigu.git;
  */
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("Hello, Git!");
-        System.out.println("Hello, Git!");
-        System.out.println("Hello, Git!");
+        System.out.println("Hello, Git1!");
+        System.out.println("Hello, Git2!");
+        System.out.println("Hello, Git3!");
     }
 }
